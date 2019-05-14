57e54b9fe78bff809ea4f88f4242691703542463
Awoseemo Akinlolu <akinloluolaposi@gmail.com>
2019-05-14 14:34:21 +0100

    My first commit

---
+++ Book.java
@@ -0,0 +1,13 @@
+	public class Book {
+		
+		private String author;
+		private int pages;
+		private boolean isHardCover;
+		private String name;
+		private String publisher; 
+		
+		public static void main(String[] args) {
+
+		}
+	
+	}
